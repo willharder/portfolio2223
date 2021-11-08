932a8882baec39f2109485076feaf194d2846c64
willharder <90637372+willharder@users.noreply.github.com>
2021-11-08 11:00:15 -0600

    Update dice.js

--- javascript/dice.js
+++ javascript/dice.js
@@ -10,29 +10,40 @@ let average;
 let totalRolls = 0;
 let grandTotal = 0;
 let largestRoll = 0;
-let winVar = 34; 
+let winVar = 34;
+var on = false;
 
 function setup() {
   createCanvas(400, 400); //height/width must equal
-  noLoop();
   colorMode(HSB);
+  //noLoop();
   diam = width / 20;
-  
 }
 
 function draw() {
-  background(random(400), random(100), random(100));
-  for (var y = 0; y < height; y += height / 2.75) {
-    for (var x = 0; x < width; x += width / 2.75) {
-      b1 = random(400);
-      b2 = random(100);
-      b3 = random(100);  
-      Dice = new Die(y + width / 160, x + width / 160);
-      Dice.show();
-      Dice.roll();
+  if (frameCount % 90 == 0) {
+    on = !on;
+    total = 0;
+    background(random(400), random(100), random(100));
+    for (var y = 0; y < height; y += height / 2.75) {
+      for (var x = 0; x < width; x += width / 2.75) {
+        b1 = random(400);
+        b2 = random(100);
+        b3 = random(100);
+        Dice = new Die(y + width / 160, x + width / 160);
+        Dice.show();
+        Dice.roll();
+      }
     }
+    totalRolls++;
+    grandTotal = grandTotal + total;
+    average = grandTotal / totalRolls;
+    if (total > largestRoll) {
+      largestRoll = total;
+    }
+    console.log("total rolls");
   }
-  
+
   strokeWeight(2);
   textSize(width / 25);
   fill("white");
@@ -40,28 +51,15 @@ function draw() {
   text("Average: " + round(average, 2), width / 20, width / 3);
   text("Grand Total: " + grandTotal, width / 1.55, width / 3);
   text("Largest Roll: " + largestRoll, width / 1.4, width / 1.45);
-  text("Total Rolls: " + totalRolls, width/2.7, height/3);
-  
-  if(total===winVar){
+  text("Total Rolls: " + totalRolls, width / 2.7, height / 3);
+
+  if (total === winVar) {
     fill("gold");
     textSize(80);
-    text("WINNER!!!!", width/256, width/1.8);
+    text("WINNER!!!!", width / 256, width / 1.8);
+  } else {
+    text("Roll a 34 for the Win!", width / 3.2, width / 1.45);
   }
-  else{
-    text("Roll a 34 for the Win!", width/3.2, width/1.45);
-  }
-}
-
-function mousePressed() {
-  background(random(400), random(100), random(100));
-  totalRolls++;
-  grandTotal = grandTotal + total;
-  average = grandTotal / totalRolls;
-  if (total > largestRoll) {
-    largestRoll = total;
-  }
-  total = 0;
-  redraw();
 }
 
 class Die {
@@ -72,7 +70,7 @@ class Die {
     total = total + this.ranNum;
   }
   roll() {
-    fill(400,0,100);
+    fill(400, 0, 100);
     if (this.ranNum == 1) {
       ellipse(this.x + height / 8, this.y + width / 8, diam, diam); //dice 1
     }
@@ -110,7 +108,9 @@ class Die {
   show() {
     strokeWeight(1);
     stroke("black");
-    fill(b1,b2,b3);
+    fill(b1, b2, b3);
     rect(this.x, this.y, height / 4, width / 4, width / 16);
   }
 }
+
+  
